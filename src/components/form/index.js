4b73d3dc6d5fb66a85555a5e55f2b205d3fa3a98
Kashif Ullah <kashifullahwebdeveloper@gmail.com>
2019-04-11 21:07:48 +0500

    Add validation functionality

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
+const required = value => value ? undefined : 'This field is required'
+
+const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+  <div className="form-group">
+    <label>{label}</label>
+      <input {...input} placeholder={label} type={type} className="form-control" />
+      {touched && ((error && <div className="invalid-feedback" style={{display: 'block'}}>{error}</div>) || (warning && <span>{warning}</span>))}
+  </div>
+)
+
 function AddItemForm(props) {
-	const { handleSubmit } = props;
+	const { handleSubmit, pristine, reset, submitting } = props
 	return <div>
 		<form onSubmit={handleSubmit}>
-			<div className="form-group">
-				<label htmlFor="name">Name</label>
-				<Field name="name" className="form-control" component="input" type="text" />
-			</div>
-			<div className="form-group">
-				<label htmlFor="price">Price</label>
-				<Field name="price" className="form-control" component="input" type="number" />
-			</div>
-			<div className="form-group">
-				<label htmlFor="image">Image</label>
-				<Field name="image" className="form-control" component="input" type="text" />
-			</div>
+				<Field 
+					name="name"
+					component={renderField} 
+					type="text"
+					validate={required}
+					label="Name" />
+				<Field 
+					name="price"
+					component={renderField} 
+					type="number"
+					validate={required}
+					label="Price" />
+				<Field 
+					name="image"
+					component={renderField} 
+					type="text"
+					validate={required}
+					label="Image" />
 			<div>
-				<button type="submit" className="btn btn-primary"> Submit </button>
+				<button type="submit" className="btn btn-primary"> Submit </button>{' '}
+				<button type="button" className="btn btn-danger" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
 			</div>
 		</form>
 	</div>
