95298386058cb23608c9406d553e5eb60fdc286a
Kashif Ullah <kashifullahwebdeveloper@gmail.com>
2019-04-11 20:14:02 +0500

    Add showing cart item in checkout page

---
+++ src/components/checkout/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Cart from '../cart';
+
+const Checkout = (props) => {
+	return <div>
+		<h2>Checkout</h2>
+		<div>
+			<Cart />
+		</div>
+	</div>
+}
+
+export default Checkout;
--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Checkout from '../components/checkout'
 
 const CheckoutPage = (props) => {
 	return <div>
-		<p>Checkout page details</p>
+		<Checkout />
 	</div>
 }
 
